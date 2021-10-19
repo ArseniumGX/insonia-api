8443d4cece345f477ae2a00766a8d147d60001f7
José Pereira Macedo <arseniumgx@gmail.com>
2021-10-19 15:19:42 -0300

    fix(cors): correção no cors para aceitar qualquer origin

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: ['https://insonia.herokuapp.com', 'http://localhost:3000'],
+    origin: "*",
     optionsSuccessStatus: 200,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 }))
