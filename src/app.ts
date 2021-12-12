0820b836531037362d7add73ca75d51b25368298
José Pereira Macêdo <45079249+ArseniumGX@users.noreply.github.com>
2021-12-12 03:19:14 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: ['https://insonia.herokuapp.co', 'http://localhost:3000'],
+    origin: ['https://insonia.herokuapp.com', 'http://localhost:3000'],
     optionsSuccessStatus: 200,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 }))
